3c63a59ecc0a2ef6d93f7d59737d35ef8047f8ac
Tony Leng <lengtk@gmail.com>
2023-04-06 11:27:53 +0900

    added header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,41 @@
+import { useState } from "react";
+import {data} from "./data";
+import logo from "./images/logo.svg";
+import {AiOutlineShoppingCart} from "react-icons/ai";
+import avatar from "./images/image-avatar.png";
 
+function Header() {
+  return (
+    <div>
+      <img src={logo} alt=""></img>
+
+      <nav>
+        <ul>
+          <li>Collections</li>
+          <li>Men</li>
+          <li>Women</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
+
+      <div>
+        <ul>
+          <li><button></button></li>
+            <AiOutlineShoppingCart />
+          <li><img src={avatar} alt=""></img></li>
+        </ul>
+      </div>
+  </div>
+  )
+}
 
 function App() {
+  const [products] = useState(data) // state value
+
   return (
     <>
-     <h1>E-Commerce Product Page</h1>
+      <Header />
     </>
   );
 }
