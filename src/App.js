dab5e2b4c4fcb384cb0e15303631a8bea7621ac0
Tony Leng <lengtk@gmail.com>
2023-04-06 12:45:06 +0900

    added product info section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {data} from "./data";
 import logo from "./images/logo.svg";
 import {AiOutlineShoppingCart} from "react-icons/ai";
 import avatar from "./images/image-avatar.png";
+import minus from "./images/icon-minus.svg";
+import plus from "./images/icon-plus.svg";
 
 function Header() {
   return (
@@ -35,6 +37,7 @@ function Header() {
 function App() {
   const [products] = useState(data) // state value
   const [value, setValue] = useState(0); // state value
+  const [amount, setAmout] =useState(0);
 
   const {mainImage} = products[value];
 
@@ -42,22 +45,50 @@ function App() {
     <>
       <Header />
 
-      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10">
+      <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10">
         <article>
-          <img src={mainImage} alt="" className="w-9/12 rounded-2xl"/>
+          <img src={mainImage} alt="" className="w-11/12 rounded-2xl"/>
 
           <ul className="flex items-center justify-start gap-5 flex-wrap mt-5">
             {products.map((item, index) => (
-            <li key={item.id} onClick={()=> setValue(index)} className={${index === value && "border-2 border-orange-400 opacity-80"}}>
-              <img src={item.thumbnail} alt="" className="w-20 rounded-xl"/>
+            <li key={item.id} onClick={()=> setValue(index)} className={
+              `${index === value && "border-2 border-orange-400 opacity-80"} border-2 rounded-2xl overflow-hidden cursor-pointer`
+              }>
+              <img src={item.thumbnail} alt="" className="w-20 rounded-2xl"/>
             </li>
           ))}
           </ul>
         </article>
 
         <article>
-            <p>test</p>
+            <h2>Sneakers Company</h2>
+            <h1>Fall Limited Edition Sneakers</h1>
+            <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
 
+            <div>
+              <ul>
+                <li>$125.00</li>
+                <li>50%</li>
+              </ul>
+
+              <p><s>$250.00</s></p>
+            </div>
+
+            <div>
+              <ul>
+                <li>
+                  <img src={minus}/>
+                </li>
+                <li>{amount}</li>
+                <li>
+                  <img src={plus}/>
+                </li>
+              </ul>
+
+              <button>
+                <AiOutlineShoppingCart/> Add to cart
+              </button>
+            </div>
         </article>
       </section>
 
