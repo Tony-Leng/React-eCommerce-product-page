fe418f3bd26d4be71acf446741977796f6ea144d
Tony Leng <lengtk@gmail.com>
2023-04-07 11:41:19 +0900

    styled product info section

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function Header() {
       <div className="flex items-center justify-start gap-4">
         <img src={logo} alt=""></img>
 
-        <nav >
+        <nav className="hidden">
           <ul className="flex items-center justify-start gap-4">
             <li>Collections</li>
             <li>Men</li>
@@ -21,6 +21,7 @@ function Header() {
             <li>Contact</li>
           </ul>
         </nav>
+      </div>
 
         <div>
           <ul className="flex items-center justify-start gap-4">
@@ -29,7 +30,6 @@ function Header() {
             <li><img src={avatar} alt="" className="width-12"></img></li>
           </ul>
         </div>
-      </div>
     </header>
   )
 }
@@ -37,17 +37,21 @@ function Header() {
 function App() {
   const [products] = useState(data) // state value
   const [value, setValue] = useState(0); // state value
-  const [amount, setAmout] =useState(0);
+  const [amount, setAmount] = useState(0);
 
   const {mainImage} = products[value];
 
+  const handleMinus = () => {
+    setAmount(amount - 1);
+    if (amount <= 0) setAmount(0);
+  }
   return (
     <>
       <Header />
 
       <section className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10">
         <article>
-          <img src={mainImage} alt="" className="w-11/12 rounded-2xl"/>
+          <img src={mainImage} alt="" className="w-full rounded-2xl"/>
 
           <ul className="flex items-center justify-start gap-5 flex-wrap mt-5">
             {products.map((item, index) => (
@@ -60,34 +64,44 @@ function App() {
           </ul>
         </article>
 
-        <article>
-            <h2>Sneakers Company</h2>
-            <h1>Fall Limited Edition Sneakers</h1>
-            <p>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
+        <article className="px-8 pb-10">
+            <h2 className="bg-slate-100 py-1 px-2 text-orange-400 uppercase tracking-wide text-sm font-bold inline-block rounded shadow mb-10">Sneakers Company</h2>
+            <h1 className="text-slate-900 mb-10 font-bold text-3xl lg:text-4xl">Fall Limited Edition Sneakers</h1>
+            <p className="text-slate-600 mb-10 leading-relaxed">These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they'll withstand everything the weather can offer.</p>
 
-            <div>
-              <ul>
-                <li>$125.00</li>
-                <li>50%</li>
+            <div className="flex flex-wrap items-center justify-between">
+              <ul className="flex items-center gap-5">
+                <li className="text-slate-900 font-bold text-2xl">$125.00</li>
+                <li className="bg-orange-100 py-1 px-2 text-orange-400 tracking-wide text-sm font-bold inline-block rounded shadow mb-10">50%</li>
               </ul>
 
-              <p><s>$250.00</s></p>
+              <p className="text-slate-600 text-sm">
+                <s>$250.00</s>
+              </p>
             </div>
 
-            <div>
-              <ul>
-                <li>
+            <div className="mt-10">
+              <ul className="flex items-center justify-between bg-slate-100 py-2 px-4 rounded shadow">
+                <li
+                  onClick={handleMinus}
+                  className="cursor-pointer"
+                >
                   <img src={minus}/>
                 </li>
                 <li>{amount}</li>
-                <li>
+                <li
+                  onClick={() => setAmount(amount + 1) }
+                  className="cursor-pointer"
+                >
                   <img src={plus}/>
                 </li>
               </ul>
 
-              <button>
-                <AiOutlineShoppingCart/> Add to cart
+              <button className="flex items-center justify-center gap-4 bg-orange-500 py-2 px-4 text-white font-bold rounded-lg shadow mt-5 w-full">
+                <AiOutlineShoppingCart
+                className="text-2xl text-slate-600"/> Add to cart
               </button>
+
             </div>
         </article>
       </section>
